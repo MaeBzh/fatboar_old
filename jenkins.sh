d69314753df39e6be35db650eee6b7baa2edde42
MaeBzh <maelenn.picaud@gmail.com>
2021-04-30 11:57:29 +0200

    update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 cp .env.docker.example .env.docker
 
-sed -i "s,$DATABASE,${DB_NAME}," .env.docker
-sed -i "s,$USER,${DB_USER}," .env.docker
-sed -i "s,$PASSWORD,${DB_PASSWORD}," .env.docker
+sed -i "s,$$DATABASE,${DB_NAME}," .env.docker
+sed -i "s,$$USER,${DB_USER}," .env.docker
+sed -i "s,$$PASSWORD,${DB_PASSWORD}," .env.docker
