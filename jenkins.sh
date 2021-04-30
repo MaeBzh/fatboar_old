f3e53df281f8cacbec0fb5dc0a315477312b5df0
MaeBzh <maelenn.picaud@gmail.com>
2021-04-30 11:56:48 +0200

    update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 cp .env.docker.example .env.docker
 
-sed -i "s,$DATABASE,${env.FATBOAR_DB_NAME}," .env.docker
-sed -i "s,$USER,${env.FATBOAR_DB_USER}," .env.docker
-sed -i "s,$PASSWORD,${env.FATBOAR_DB_PASSWORD}," .env.docker
+sed -i "s,$DATABASE,${DB_NAME}," .env.docker
+sed -i "s,$USER,${DB_USER}," .env.docker
+sed -i "s,$PASSWORD,${DB_PASSWORD}," .env.docker
