8aea3f387a9a3ab636553c439866a2adbecb1c6e
MaeBzh <maelenn.picaud@gmail.com>
2021-05-12 17:39:23 +0200

    test

--- fatboar-api/src/users/users.controller.spec.ts
+++ fatboar-api/src/users/users.controller.spec.ts
@@ -15,7 +15,7 @@ describe('UsersController', () => {
             const users = [
                 {
                 id: 1,
-                firstname: 'Jojo',
+                firstname: 'sdffg',
                 lastname: 'lepoireau',
                 email: 'test@user.com',
                 phone: '1234567890'
