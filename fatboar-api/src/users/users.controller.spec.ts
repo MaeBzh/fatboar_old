c1c2a9c9f9b2573b1f6f70f6b0d777640897af53
MaeBzh <maelenn.picaud@gmail.com>
2021-05-12 15:44:59 +0200

    test tag release

--- fatboar-api/src/users/users.controller.spec.ts
+++ fatboar-api/src/users/users.controller.spec.ts
@@ -19,7 +19,7 @@ describe('UsersController', () => {
             const users = [
                 {
                 id: 1,
-                firstname: 'Bruno',
+                firstname: 'Charlot',
                 lastname: 'lepoireau',
                 email: 'test@user.com',
                 phone: '1234567890'
