97528b7b86a95546ded7b96193653cd5f909386a
MaeBzh <maelenn.picaud@gmail.com>
2021-05-12 18:13:58 +0200

    test deploy

--- fatboar-api/src/users/users.controller.spec.ts
+++ fatboar-api/src/users/users.controller.spec.ts
@@ -15,7 +15,7 @@ describe('UsersController', () => {
             const users = [
                 {
                 id: 1,
-                firstname: 'wfsv',
+                firstname: 'wdfvw',
                 lastname: 'lepoireau',
                 email: 'test@user.com',
                 phone: '1234567890'
